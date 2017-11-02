de68eb8c1a0cec28285a70c502bad845db4521fe
Jasper Gilhuis (Xpirit) <jgilhuis@xpirit.com>
2017-11-02 17:16:07 +0100

    Package Consumer Repository

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Package.Mammal m = new Package.Mammal();
+            Package.Reptile r = new Package.Reptile();
+
+            if (m.BloodType == Package.Enum.BloodTypeEnum.Warm)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Warm...");
+            }
+            if (r.BloodType == Package.Enum.BloodTypeEnum.Cold)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Cold...");
+            }
+
+
+        }
+    }
+}
